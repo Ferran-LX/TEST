b0b4f9fb68f50016ebdce8e51daa4e60b25e90b7
Ferran-LX <115022293+Ferran-LX@users.noreply.github.com>
2023-02-14 21:04:25 +0100

    Update Hi.c

--- Hi.c
+++ Hi.c
@@ -1,14 +1,14 @@
- Simple C program to display Hello World
+ //Simple C program to display Hello World
 
- Header file for input output functions
+ //Header file for input output functions
 #include stdio.h
 
- main function -
- where the execution of program begins
+// main function -
+// where the execution of program begins
 int main()
 {
 
-	 prints hello world
+	// prints hello world
 	printf(Hello World);
 
 	return 0;
